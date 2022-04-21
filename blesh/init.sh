02bf5faaf8f5c2f4840c66fec27adbad4faa4075
Nebow <81814662+Nebow@users.noreply.github.com>
2022-04-21 15:45:45 +0000

    Update init.sh

--- blesh/init.sh
+++ blesh/init.sh
@@ -1,3 +1,8 @@
 bleopt complete_auto_delay=100
 ble-face syntax_error=fg=203
 ble-face auto_complete=fg=242
+ble-face command_file=fg=003
+ble-face command_builtin=fg=003
+ble-face command_alias=fg=003
+ble-face filename_directory=fg=003
+ble-face filename_other=fg=003

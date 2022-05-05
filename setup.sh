e1ae572ee2a943248ca7f0fa52faf5dc456ebb6a
Nebow <81814662+Nebow@users.noreply.github.com>
2022-05-05 15:03:30 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo pacman --noconfirm -S foot ranger nano rofi wofi zathura waybar ttf-font-awesome htop swaylock zathura-pdf-mupdf xdg-utils terminus-font sway firefox noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts ttf-liberation ttf-dejavu ttf-roboto slurp wl-clipboard jq grim pamixer
+sudo pacman --noconfirm -S foot ranger nano rofi wofi zathura waybar ttf-font-awesome htop swaylock zathura-pdf-mupdf xdg-utils terminus-font sway firefox noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts ttf-liberation ttf-dejavu ttf-roboto slurp wl-clipboard jq grim pamixer brightnessctl
 
 
 git clone https://aur.archlinux.org/yay.git

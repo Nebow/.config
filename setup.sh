ff296f20026ec6df2ae82be00fb25f1c29c95ff9
Nebow <81814662+Nebow@users.noreply.github.com>
2022-05-07 11:18:15 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-sudo pacman --noconfirm -S foot ranger nano rofi wofi zathura waybar ttf-font-awesome htop swaylock zathura-pdf-mupdf xdg-utils terminus-font sway firefox noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts ttf-liberation ttf-dejavu ttf-roboto slurp wl-clipboard jq grim pamixer brightnessctl nomacs pngquant mpv
+sudo pacman --noconfirm -S foot nano wofi zathura waybar ttf-font-awesome htop swaylock zathura-pdf-mupdf xdg-utils terminus-font sway firefox noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts ttf-liberation ttf-dejavu ttf-roboto slurp wl-clipboard jq grim pamixer brightnessctl nomacs pngquant mpv
 
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg --noconfirm -si
+
+yay -S --noconfirm ranger-git rofi-lbonn-wayland
 
 
 touch ~/.hushlogin
